a28d97012f5d8faa82f60dfec4dee53c5b5fe7d5
Patricio Cano <suprnova32@gmail.com>
2012-10-22 12:14:32 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cotopaxi::Application.config.secret_token = 'a4ddd4a8f6fa88760794b926b1cd6d711afc47761b8e71fd13243b80183cef1204cbdea4cf47ea289dcdf061b49c1323999ef5f393b49367a5887f3ae65cb285'
